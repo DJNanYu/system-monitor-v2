5cafa737c99653e8c1757d60ebccc2cf378836da
DJNanYu <2986945687@qq.com>
2026-01-18 10:00:00 -0500

    feat: initial draft of system-monitor-v2

---
+++ README.md
@@ -0,0 +1,5 @@
+# system-monitor-v2
+
+Enhanced Native Monitor V2. 增强型原生监控工具V2
+
+Initial development of system-monitor-v2.
---
+++ main.cpp
@@ -0,0 +1,9 @@
+// Draft version for system-monitor-v2
+
+
+#include <jni.h>
+#include <string>
+#include <android/log.h>
+
+#define LOG_TAG "NativeSecurity"
+#defi...
